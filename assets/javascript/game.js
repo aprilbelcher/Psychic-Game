ff7f86572ec4b2c8a12193d5367d5a67c2b06217
April Denise Belcher <aprildenise@Aprils-MacBook-Pro.local>
2017-10-25 00:30:27 -0700

    pseudo coding input

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+var computerChoices = [>= 65 && event.keyCode <= 90]; //shows the charcode options
+var win = 0;
+var loss = 0;
+var guess = 10
+
+document.onkeyup = function(event) { //when keys are clicked this is what happens
+
+	var userGuess = event.key; 
+
+	var computerGuess = computerChoices[Math.floor(Math.random()*
+		computerChoices.length)];
+
+	if(userGuess === (>= 65 && event.keyCode <= 90) {
+
+		if ((userGuess === computerGuess) {
+			win++}{guess left go back to 10}
+		};
+		else if (userGuess !==computerGuess) {
+			lose++
+		}{-guessLeft};
+
+		else if (guessLeft === 0){game over}
+
+	var results = "You pikced" + userGuess + "CPU picked" + computerGuess;
+		computerGuess;
+	document.getElementById("game").textcontent = results
+	} //find the right codes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
