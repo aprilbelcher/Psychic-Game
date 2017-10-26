eae506dfa391ef525986b7bbbae2c825f57a0c20
April Denise Belcher <aprildenise@Aprils-MacBook-Pro.local>
2017-10-25 17:48:45 -0700

    update after school

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,36 @@
-var computerChoices = [>= 65 && event.keyCode <= 90]; //shows the charcode options
+
 var win = 0;
 var loss = 0;
-var guess = 10
+var guess = 10;
 
+
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	var computerGuess = computerChoices[Math.floor(Math.random()*
+		computerChoices.length)];
+	console.log(computerGuess);
 document.onkeyup = function(event) { //when keys are clicked this is what happens
 
 	var userGuess = event.key; 
+	
+	var i = event.keyCode
 
-	var computerGuess = computerChoices[Math.floor(Math.random()*
-		computerChoices.length)];
+    var results = "You picked" + userGuess + "CPU picked" + computerGuess;
 
-	if(userGuess === (>= 65 && event.keyCode <= 90) {
+	 if(userGuess === computerGuess) {
+			win++;
+			guess = 10;
+			document.getElementById("game").innerHTML = results
+		}
+		else if (userGuess !== computerGuess){
+			guess--;
+			console.log(guess);
+			if (guess === 0 ){guess = 10;
+				loss++;
+			document.getElementById("game").innerHTML = results
+			}
 
-		if ((userGuess === computerGuess) {
-			win++}{guess left go back to 10}
-		};
-		else if (userGuess !==computerGuess) {
-			lose++
-		}{-guessLeft};
+		}
 
-		else if (guessLeft === 0){game over}
-
-	var results = "You pikced" + userGuess + "CPU picked" + computerGuess;
-		computerGuess;
-	document.getElementById("game").textcontent = results
-	} //find the right codes
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	var results = "You picked" + userGuess + "CPU picked" + computerGuess;
+	
 }
---
+++ assets/javascript/sample.js
@@ -0,0 +1,3 @@
+var bvdisvbds = {
+	property : "value"
+}
