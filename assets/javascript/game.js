09fd98d05505ab11779dfbc9afa93375c8f426db
April Denise Belcher <aprildenise@Aprils-MacBook-Pro.local>
2017-10-25 21:55:55 -0700

    added corrections and notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,50 @@
 
-var win = 0;
-var loss = 0;
-var guess = 10;
+var wins = 0;
+var losses = 0;
+var guessesleft = 10;
+
 
 
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	var computerGuess = computerChoices[Math.floor(Math.random()*
 		computerChoices.length)];
-	console.log(computerGuess);
-document.onkeyup = function(event) { //when keys are clicked this is what happens
+	
+document.onkeyup = function(event) { 
 
 	var userGuess = event.key; 
+	//alert(userGuess);
+	
+    
+    
+
 	
 	var i = event.keyCode
 
-    var results = "You picked" + userGuess + "CPU picked" + computerGuess;
+    var results =  userGuess ;
+       console.log(results);
+    //document.getElementById("game").innerHTML = results
+    
 
-	 if(userGuess === computerGuess) {
-			win++;
-			guess = 10;
-			document.getElementById("game").innerHTML = results
+	 if(userGuess === computerGuess) { //user guessed right
+			wins++;
+			guessesleft = 10;
+			document.getElementById("wins").innerHTML = wins
+			
 		}
-		else if (userGuess !== computerGuess){
-			guess--;
-			console.log(guess);
-			if (guess === 0 ){guess = 10;
-				loss++;
+		else if (userGuess !== computerGuess){ //user guessed wrong
+			guessesleft--;
+			document.getElementById("guessesLeft").innerHTML = guessesleft;
+			console.log("guessleft:"+guessesleft);
+			
+		
+			if (guessesleft === 0 ){ guessesleft = 10;
+				losses++;
+				document.getElementById("losses").innerHTML = losses;
 			document.getElementById("game").innerHTML = results
 			}
 
 		}
 
-	var results = "You picked" + userGuess + "CPU picked" + computerGuess;
+	
 	
 }
