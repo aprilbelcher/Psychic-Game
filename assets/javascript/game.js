6657b8f1b86eedbd97f1199c8c5407592e3027a5
April Denise Belcher <aprildenise@Aprils-MacBook-Pro.local>
2017-10-26 23:29:26 -0700

    dome

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,42 +6,43 @@ var guesses = 10;
 var guessedLetters = [];
 
 //press key function
-document.onkeyup = function(event) { 
+document.onkeyup = function(event) {
 
-	var userGuess = event.key; 
+	var userGuess = event.key;
 
 	//array of computer's choices
 	var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-		
+
 	var computerGuess = computerChoices[Math.floor(Math.random()*
 			computerChoices.length)];
 
 		//first condition
 		if (userGuess || computerGuess) {
-			
+
 			//user guessed correctly
 			if (userGuess === computerGuess) {
-			wins++;
+			  wins++;
 			}
 			//user guessed incorrect
 			else if (userGuess !== computerGuess) {
-			guessesLeft--;guessedLetters++;
+			  guessesLeft--;
+        guessedLetters.push(userGuess);
 			}
 			//score is 0
 			if (guessesLeft === 0 ){
-					losses++; (guessesLeft = 10);
+			  losses++; (guessesLeft = 10);
 			}
-		
+
     var results =
     	"<p>Wins: " + wins + "</p>" +
     	"<p>Losses " + losses + "</p>" +
     	"<p>Guesses Left: " + guessesLeft + "</p>" +
-    	"<p>Your Guesses so far: " + guessedLetters + "</p>"; 
+    	"<p>Your Guesses so far: " + guessedLetters + "</p>";
 
-    	document.getElementById("game")innerHTML = results;
+    	document.getElementById("game").innerHTML = results;
 	}
 };
 
 
-	
-	
+
+
