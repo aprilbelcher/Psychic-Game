e9d6ac5deb7cca7066600e4c44be56abafc94728
April Denise Belcher <aprildenise@Aprils-MacBook-Pro.local>
2017-10-26 21:46:29 -0700

    aaaaaaaalmooost there >;(

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,47 @@
-
+//variables to store guess selectora
 var wins = 0;
 var losses = 0;
-var guessesleft = 10;
+var guessesLeft = 10;
+var guesses = 10;
+var guessedLetters = [];
 
-
-
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-	var computerGuess = computerChoices[Math.floor(Math.random()*
-		computerChoices.length)];
-	
+//press key function
 document.onkeyup = function(event) { 
 
 	var userGuess = event.key; 
-	//alert(userGuess);
-	
-    
-    
 
-	
-	var i = event.keyCode
-
-    var results =  userGuess ;
-       console.log(results);
-    //document.getElementById("game").innerHTML = results
-    
-
-	 if(userGuess === computerGuess) { //user guessed right
-			wins++;
-			guessesleft = 10;
-			document.getElementById("wins").innerHTML = wins
-			
-		}
-		else if (userGuess !== computerGuess){ //user guessed wrong
-			guessesleft--;
-			document.getElementById("guessesLeft").innerHTML = guessesleft;
-			console.log("guessleft:"+guessesleft);
-			
+	//array of computer's choices
+	var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 		
-			if (guessesleft === 0 ){ guessesleft = 10;
-				losses++;
-				document.getElementById("losses").innerHTML = losses;
-			document.getElementById("game").innerHTML = results
+	var computerGuess = computerChoices[Math.floor(Math.random()*
+			computerChoices.length)];
+
+		//first condition
+		if (userGuess || computerGuess) {
+			
+			//user guessed correctly
+			if (userGuess === computerGuess) {
+			wins++;
 			}
+			//user guessed incorrect
+			else if (userGuess !== computerGuess) {
+			guessesLeft--;guessedLetters++;
+			}
+			//score is 0
+			if (guessesLeft === 0 ){
+					losses++; (guessesLeft = 10);
+			}
+		
+    var results =
+    	"<p>Wins: " + wins + "</p>" +
+    	"<p>Losses " + losses + "</p>" +
+    	"<p>Guesses Left: " + guessesLeft + "</p>" +
+    	"<p>Your Guesses so far: " + guessedLetters + "</p>"; 
 
-		}
+    	document.getElementById("game")innerHTML = results;
+	}
+};
+
 
 	
 	
-}
